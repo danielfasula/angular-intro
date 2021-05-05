b1c37c3167364288c4ff7231eac6f143f59a8fa7
Daniel Fasula <danielfasula@gmail.com>
2021-05-05 17:34:33 -0600

    init

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-footer",
+  template: `
+    <footer class="footer">
+      <div class="container content has-text-centered">
+        <p>Made with 💖 by Daniel</p>
+      </div>
+    </footer>
+  `,
+  styles: [],
+})
+export class FooterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
